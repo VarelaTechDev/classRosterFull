91c4b9b0b0523cf1b373e9e07a960009f38598d0
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-14 08:55:54 -0700

    first commit

---
+++ src/main/java/com/av/classrosterFull/ClassrosterFullApplication.java
@@ -0,0 +1,13 @@
+package com.av.classrosterFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassrosterFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassrosterFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/av/classrosterFull/ClassrosterFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.av.classrosterFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassrosterFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
