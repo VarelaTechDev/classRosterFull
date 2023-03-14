a6047bf0dc94c62da74ad5670cee0ce726541976
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-14 11:05:33 -0700

    Student DAO Implementation

--- src/main/java/com/av/classrosterFull/Dao/TeacherDao.java
+++ src/main/java/com/av/classrosterFull/Dao/TeacherDao.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TeacherDao {
     Teacher getTeacherById(int id);
     List<Teacher> getAllTeachers();
