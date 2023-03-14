95bea2fcb91cc32d71a2957490767dd38e330c90
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-14 11:00:04 -0700

    Teacher DAO Implementation and of course we place the mapper at the END of the file and we import the SpringFramework RowMapper

--- src/main/java/com/av/classrosterFull/Dao/TeacherDao.java
+++ src/main/java/com/av/classrosterFull/Dao/TeacherDao.java
@@ -1,9 +1,11 @@
 package com.av.classrosterFull.Dao;
 
 import com.av.classrosterFull.Entities.Teacher;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TeacherDao {
     Teacher getTeacherById(int id);
     List<Teacher> getAllTeachers();
